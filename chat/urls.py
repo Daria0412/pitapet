47498254be30906f6a53f92351667e7fb3d09cef
daria0412 <s2018w29@icloud.com>
2020-05-09 01:54:36 +0900

    catch index url error

--- chat/urls.py
+++ chat/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.main, name='main'),
     url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
 ]
