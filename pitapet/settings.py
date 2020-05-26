a5d8cfdce46019b4a2a956846231ac943b396c28
daria0412 <s2018w29@icloud.com>
2020-05-27 01:31:29 +0900

    add allowed_hosts

--- pitapet/settings.py
+++ pitapet/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '5_#%_mue1zhq)k0oc0-*7f=ewo@c$9vln%p2@kt73gec7yh3%!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
