631958d0af5ddff3bd12352e217645b125231351
daria0412 <s2018w29@icloud.com>
2020-07-08 17:20:45 +0900

    default.png

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
 class User(models.Model):
     member_id = models.CharField(db_column='member_id',primary_key=True,max_length=200,blank=False)
     pwd = models.CharField(db_column='pwd',max_length=200,blank=False)
@@ -14,7 +13,7 @@ class User(models.Model):
     addr_city = models.CharField(db_column='addr_city',max_length=150,blank=False)
     addr_gu = models.CharField(db_column='addr_gu',max_length=100,blank=False)
     profile = models.IntegerField(db_column='profile',blank=False)
-    img_url = models.CharField(db_column='img_url',max_length=100,blank=False)
+    img_url = models.CharField(db_column='img_url',max_length=100,blank=False,default='default.png')
 
     class Meta:
         managed = False
